22b4e96768757965563a9d58eefc673e1c97022b
Cristian Bernal <cristianbermen@gmail.com>
2023-09-21 12:38:01 +0200

    21/09/2023

---
+++ Datos/helloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+/**
+ *
+ * @author a23cristianbm
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
