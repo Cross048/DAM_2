767fec3670c49fb9540710137abdfd960854f371
Cristian Bernal <115544159+Cross048@users.noreply.github.com>
2023-10-01 17:04:09 +0000

    Some exercices done

--- Datos/EjerciciosRepaso/src/ejercicio2/Main.java
+++ Datos/EjerciciosRepaso/src/ejercicio2/Main.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicio2;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Cristian Bernal Méndez
  */
 public class Main {
-    
+    /**
+     * Cifrado César
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Introduzca cadena de texto:");
+        String cadenaACifrar = sc.nextLine();
+
+        System.out.println(cifradoCesar(cadenaACifrar));
+    }
+    /**
+     * Realiza el cifrado César
+     * @param cadenaACifrar
+     * @return cadenaCifrada
+     */
+    public static String cifradoCesar (String cadenaACifrar) {
+        StringBuilder cadenaCifrada = new StringBuilder();
+
+        for (int i = 0; i < cadenaACifrar.length(); i++) {
+            char caracter = cadenaACifrar.charAt(i);
+            if (Character.isLetter(caracter)) {
+                // Ignora la mayúscula o minúscula
+                char base = Character.isLowerCase(caracter) ? 'a' : 'A';
+                // Realiza el cifrado César
+                char cifrado = (char) (((caracter - base + 3) % 26) + base);
+                cadenaCifrada.append(cifrado);
+            } else {
+                // En caso de espacios o signos
+                cadenaCifrada.append(caracter);
+            }
+        }
+
+        return cadenaCifrada.toString();
+    }
+    // Escaner
+    static Scanner sc = new Scanner(System.in);
 }
