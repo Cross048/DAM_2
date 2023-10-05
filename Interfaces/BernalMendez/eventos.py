e0c5b6d1a02aebfcfa36dfc6f755530865b53bb1
Cristian Bernal <cristianbermen@gmail.com>
2023-10-05 14:23:19 +0200

    Added Calendar feature

--- Interfaces/BernalMendez/eventos.py
+++ Interfaces/BernalMendez/eventos.py
@@ -3,6 +3,18 @@ import var
 class Eventos():
     def salir(self):
         try:
-            pass
+            sys.exit(0)
         except Exception as eror:
             print(error, "en módulo eventos")
+
+    def abrirCalendar(self):
+        try:
+            var.calendar.show()
+        except Exception as error:
+            print('error en abrir calendar', error)
+    
+    def acercade():
+        try:
+            pass
+        except Exception as eror:
+            print("error abrir ventana acerca de", error)
--- Interfaces/BernalMendez/main.py
+++ Interfaces/BernalMendez/main.py
@@ -1,25 +1,46 @@
 # import eventos
-from MainWindow import *
+from MainWindow_ui import *
 import sys, var, eventos
+from Calendar_ui import *
+from datetime import datetime
 
 # Instalar PyQt6 con
 # pip install pyqt6
+
+class Calendar(QtWidgets.QDialog):
+    def __init__(self):
+        super(Calendar, self).__init__()
+        var.calendar = Ui_dlgCalendar()
+        var.calendar.setupUi(self)
+        dia = datetime.now().day
+        mes = datetime.now().month
+        ano = datetime.now().year
+        '''
+        zona de eventos de botones
+        '''
+        # Abrir Calendario
+        var.ui.btnCalendar.clicked.connect(eventos.Eventos.abrirCalendar)
 
 class Main(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Main, self).__init__()
         var.ui = Ui_mainWindow()
-        var.ui.setupUi(self) #encargado la interfaz
-
+        var.ui.setupUi(self) # Encargado la interfaz
+        var.calendar = Calendar() # Ventana del Calendario
         '''
         zona de eventos de botones
         '''
-        var.ui.btnSalir.clicked.connect(eventos.Eventos.salir)
+        # Cerrar programa
+        # var.ui.btnSalir.clicked.connect(eventos.Eventos.salir)
+        '''
+        zona de eventos de botones
+        '''
+        # Cerrar programa
+        # var.ui.actionSalir.triggered.connect(eventos.Eventos.salir)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     window = Main()
     window.show()
     sys.exit(app.exec())
-
