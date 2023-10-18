dd76f463750b675ab951045eef8bd0e198dff152
Cristian Bernal <cristianbermen@gmail.com>
2023-10-18 10:35:30 +0200

    Update Practica3.java

--- Datos/FicherosAula/src/ficherosaula/Practica3.java
+++ Datos/FicherosAula/src/ficherosaula/Practica3.java
@@ -14,8 +14,7 @@ public class Practica3 {
      * fichero indicado desde línea de comandos. Los contenidos del fichero se
      * leen en bloques de 32 bytes, y el contenido de cada bloque se escribe en
      * una línea de texto. Los bytes se escriben en hexadecimal (base 16) y, por
-     * tanto, cada byte se escribe utilizando dos caracteres. El programa
-     * muestra como máximo los primeros 2 kilobytes (MAX_BYTES=2048).
+     * tanto, cada byte se escribe utilizando dos caracteres.
      * @param args 
      */
     public static void main(String[] args) {
