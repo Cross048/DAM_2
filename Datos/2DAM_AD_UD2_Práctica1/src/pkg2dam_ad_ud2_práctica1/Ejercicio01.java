61942b54ebe5cec27ff24c6e134e954b477c75bb
Cristian Bernal <cristianbermen@gmail.com>
2023-11-23 11:05:36 +0100

    Updated Ejercicio01

--- Datos/2DAM_AD_UD2_Práctica1/src/pkg2dam_ad_ud2_práctica1/Ejercicio01.java
+++ Datos/2DAM_AD_UD2_Práctica1/src/pkg2dam_ad_ud2_práctica1/Ejercicio01.java
@@ -1,20 +1,133 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg2dam_ad_ud2_práctica1;
 
-/**
- *
- * @author Cristian Bernal Méndez
- */
-public class Ejercicio01 {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Scanner;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class Ejercicio01 {
+    // Inicializa variables
+    private static PreparedStatement consulta = null;
+    private static Connection connection = null;
+    private static ResultSet result = null;
+
+    // Credenciales: BBDD, Usuario y Contraseña
+    private static String url = "jdbc:mysql://localhost:3306/empleados";
+    private static String user = "Cristian";
+    private static String password = "admin";
+    
     /**
-     * @param args the command line arguments
+     * Partiendo del ejemplo visto en las diapositivas de la UD2. BD empresa, diseña una 
+     * aplicación Java que se adapte al paradigma modelo-vista-controlador estudiado en clase.
+     * @param args
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // Carga el driver de MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establece la conexión
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexion establecida!");
+
+            // 1. Consultar todos los empleados.
+            mostrarEmpleados();
+            // 2. Consultar empleados por nif/dni.
+            mostrarSegunNIF();
+            // 3. Consultar empleados que tengan un salario superior al introducido por el usuario.
+            // TODO: Hacer mostrarSalarioSuperior()
+            mostrarSalarioSuperior();
+            // 4. Consultar empleados que tengan un salario igual o inferior al introducido por el usuario.
+            // TODO: Hacer mostrarSalarioInferior()
+            mostrarSalarioInferior();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally { // Cerrar las conexiones
+            try {
+                if (consulta != null) { consulta.close(); }
+                if (result != null) { result.close(); }
+                if (connection != null) { connection.close(); }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
+
+    /* Recorre y muestra todos los empleados */
+    public static void mostrarEmpleados() {
+        try {
+            String sql = "SELECT * FROM empleados.empleado;";
+
+            consulta = connection.prepareStatement(sql);
+            result = consulta.executeQuery();
+
+            while (result.next()) {
+                int NSS = result.getInt(1);
+                String Nombre = result.getString(2);
+                String Apel1 = result.getString(3);               
+                String Apel2 = result.getString(4);
+                char Sexo = result.getString(5).charAt(0);
+                String Direccion = result.getString(6);
+                Date Nacimiento = result.getDate(7);
+                int Salario = result.getInt(8);
+                int Deparmanteo = result.getInt(9);
+                int Supervisor = result.getInt(10);
+                String NIF = result.getString(11);
+
+                Empleado empleado = new Empleado(NSS, Nombre, Apel1, Apel2, Sexo, Direccion, Nacimiento, Salario, Deparmanteo, Supervisor, NIF);
+                System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+                System.out.println(empleado.toString());
+            }
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* Muesta los empleados que tengan el NIF indicado */
+    public static void mostrarSegunNIF() {
+        try {
+            System.out.println("Dime el NIF a buscar: ");
+            String DNI = sc.nextLine();
+            String sql = "SELECT * FROM empleados.empleado WHERE NIF=?;";
+
+            consulta = connection.prepareStatement(sql);
+            consulta.setString(1, DNI); // Configura el parámetro
+            result = consulta.executeQuery();
+
+            while (result.next()) {
+                int NSS = result.getInt(1);
+                String Nombre = result.getString(2);
+                String Apel1 = result.getString(3);               
+                String Apel2 = result.getString(4);
+                char Sexo = result.getString(5).charAt(0);
+                String Direccion = result.getString(6);
+                Date Nacimiento = result.getDate(7);
+                int Salario = result.getInt(8);
+                int Deparmanteo = result.getInt(9);
+                int Supervisor = result.getInt(10);
+                String NIF = result.getString(11);
+
+                Empleado empleado = new Empleado(NSS, Nombre, Apel1, Apel2, Sexo, Direccion, Nacimiento, Salario, Deparmanteo, Supervisor, NIF);
+                System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+                System.out.println(empleado.toString());
+            }
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void mostrarSalarioSuperior() {}
+
+    public static void mostrarSalarioInferior() {}
+
+    /* Escáner */
+    static Scanner sc = new Scanner(System.in);
 }
---
+++ Datos/2DAM_AD_UD2_Práctica1/src/pkg2dam_ad_ud2_práctica1/Empleado.java
@@ -0,0 +1,210 @@
+package pkg2dam_ad_ud2_práctica1;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Empleado {
+    int NSS;
+    String Nombre;
+    String Apel1;               
+    String Apel2;
+    char Sexo;
+    String Direccion;
+    Date Nacimiento;
+    int Salario;
+    int Deparmanteo;
+    int Supervisor;
+    String NIF;
+
+    public Empleado() {
+    }
+
+    public Empleado(int NSS, String Nombre, String Apel1, String Apel2, char Sexo, String Direccion, Date Nacimiento, int Salario, int Deparmanteo, int Supervisor, String NIF) {
+        this.NSS = NSS;
+        this.Nombre = Nombre;
+        this.Apel1 = Apel1;
+        this.Apel2 = Apel2;
+        this.Sexo = Sexo;
+        this.Direccion = Direccion;
+        this.Nacimiento = Nacimiento;
+        this.Salario = Salario;
+        this.Deparmanteo = Deparmanteo;
+        this.Supervisor = Supervisor;
+        this.NIF = NIF;
+    }
+
+    public int getNSS() {
+        return this.NSS;
+    }
+
+    public void setNSS(int NSS) {
+        this.NSS = NSS;
+    }
+
+    public String getNombre() {
+        return this.Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApel1() {
+        return this.Apel1;
+    }
+
+    public void setApel1(String Apel1) {
+        this.Apel1 = Apel1;
+    }
+
+    public String getApel2() {
+        return this.Apel2;
+    }
+
+    public void setApel2(String Apel2) {
+        this.Apel2 = Apel2;
+    }
+
+    public char getSexo() {
+        return this.Sexo;
+    }
+
+    public void setSexo(char Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public String getDireccion() {
+        return this.Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public Date getNacimiento() {
+        return this.Nacimiento;
+    }
+
+    public void setNacimiento(Date Nacimiento) {
+        this.Nacimiento = Nacimiento;
+    }
+
+    public int getSalario() {
+        return this.Salario;
+    }
+
+    public void setSalario(int Salario) {
+        this.Salario = Salario;
+    }
+
+    public int getDeparmanteo() {
+        return this.Deparmanteo;
+    }
+
+    public void setDeparmanteo(int Deparmanteo) {
+        this.Deparmanteo = Deparmanteo;
+    }
+
+    public int getSupervisor() {
+        return this.Supervisor;
+    }
+
+    public void setSupervisor(int Supervisor) {
+        this.Supervisor = Supervisor;
+    }
+
+    public String getNIF() {
+        return this.NIF;
+    }
+
+    public void setNIF(String NIF) {
+        this.NIF = NIF;
+    }
+
+    public Empleado NSS(int NSS) {
+        setNSS(NSS);
+        return this;
+    }
+
+    public Empleado Nombre(String Nombre) {
+        setNombre(Nombre);
+        return this;
+    }
+
+    public Empleado Apel1(String Apel1) {
+        setApel1(Apel1);
+        return this;
+    }
+
+    public Empleado Apel2(String Apel2) {
+        setApel2(Apel2);
+        return this;
+    }
+
+    public Empleado Sexo(char Sexo) {
+        setSexo(Sexo);
+        return this;
+    }
+
+    public Empleado Direccion(String Direccion) {
+        setDireccion(Direccion);
+        return this;
+    }
+
+    public Empleado Nacimiento(Date Nacimiento) {
+        setNacimiento(Nacimiento);
+        return this;
+    }
+
+    public Empleado Salario(int Salario) {
+        setSalario(Salario);
+        return this;
+    }
+
+    public Empleado Deparmanteo(int Deparmanteo) {
+        setDeparmanteo(Deparmanteo);
+        return this;
+    }
+
+    public Empleado Supervisor(int Supervisor) {
+        setSupervisor(Supervisor);
+        return this;
+    }
+
+    public Empleado NIF(String NIF) {
+        setNIF(NIF);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Empleado)) {
+            return false;
+        }
+        Empleado empleado = (Empleado) o;
+        return NSS == empleado.NSS && Objects.equals(Nombre, empleado.Nombre) && Objects.equals(Apel1, empleado.Apel1) && Objects.equals(Apel2, empleado.Apel2) && Sexo == empleado.Sexo && Objects.equals(Direccion, empleado.Direccion) && Objects.equals(Nacimiento, empleado.Nacimiento) && Salario == empleado.Salario && Deparmanteo == empleado.Deparmanteo && Supervisor == empleado.Supervisor && Objects.equals(NIF, empleado.NIF);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(NSS, Nombre, Apel1, Apel2, Sexo, Direccion, Nacimiento, Salario, Deparmanteo, Supervisor, NIF);
+    }
+
+    @Override
+    public String toString() {
+        return "NSS: " + getNSS() + " | " +
+            "Nombre: " + getNombre() + " | " +
+            "Apellido 1: " + getApel1() + " | " +
+            "Apellido 2: " + getApel2() + " | " +
+            "Sexo: " + getSexo() +
+            "\nDireccion: " + getDireccion() + " | " +
+            "Nacimiento: " + getNacimiento() + " | " +
+            "Salario: " + getSalario() + " | " +
+            "Deparmanteo: " + getDeparmanteo() + " | " +
+            "Supervisor: " + getSupervisor() + " | " +
+            "NIF: " + getNIF();
+    }
+    
+}
