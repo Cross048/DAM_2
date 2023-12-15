0637ca906958f5742fdcd7d01989fb3aeb31b7ca
Cristian Bernal <cristianbermen@gmail.com>
2023-12-15 12:26:08 +0100

    Update Mayusculas.java

--- PSP/UD1/2- MAYUSCULAS/Mayusculas/src/mayusculas/Mayusculas.java
+++ PSP/UD1/2- MAYUSCULAS/Mayusculas/src/mayusculas/Mayusculas.java
@@ -3,7 +3,6 @@ package mayusculas;
 import java.io.*;
 
 public class Mayusculas {
-
   public static void main(String[] args) {
     String line;
 
@@ -20,13 +19,12 @@ public class Mayusculas {
       
       // Para salir del bucle teclea ff
       // vamos a procesar una..n cadenas
-      
       do{
         line = in.readLine();
         
         ps.println(line); //envio lo leido por teclado al proceso hijo
         ps.flush(); // IMP: Comprueba envío de datos
-        line = br.readLine();//intento leer los datos que el hijo me deja
+        line = br.readLine(); //intento leer los datos que el hijo me deja
         if (!(line.equals("kk"))) {
           System.out.println(line);
         }
@@ -36,7 +34,5 @@ public class Mayusculas {
     } catch (IOException e) {
       System.out.println("Error ocurrió durante la ejecución " + "Descripción del error: " + e.getMessage());
     }
-
   }
-
 }
