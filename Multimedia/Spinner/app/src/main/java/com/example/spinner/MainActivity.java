70e2409e15150ea30f4a66c53fa1e8b8d9fd8377
Cross048 <cristianbermen@gmail.com>
2024-01-23 12:55:24 +0100

    Update MainActivity.java

--- Multimedia/Spinner/app/src/main/java/com/example/spinner/MainActivity.java
+++ Multimedia/Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner spPlanetas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //escuchador del spinner
+        spPlanetas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String planeta = spPlanetas.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 }
