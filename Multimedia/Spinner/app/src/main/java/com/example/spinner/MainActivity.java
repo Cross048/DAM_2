2d4b12554b754325f40346dcc69ce670ac267f9e
Cristian Bernal <cristianbermen@gmail.com>
2024-01-29 15:10:40 +0100

    Update MainActivity.java

--- Multimedia/Spinner/app/src/main/java/com/example/spinner/MainActivity.java
+++ Multimedia/Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner spPlanetas;
+    private Spinner spProvincia;
+    private Spinner spLocalidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //escuchador del spinner
-        spPlanetas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spProvincia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String planeta = spPlanetas.getSelectedItem().toString();
+                String planeta = spProvincia.getSelectedItem().toString();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
+
+        String[] arrayPlanetas= {"Mercurio", "Venus", "Tierra", "Marte", "Júpiter",
+                "Saturno","Urano","Neptuno"};
+
+        //creamos el elemento adaptador
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item, arrayPlanetas);
+        //asignamos al Spinner el adaptador con los datos ya cargados
+        spPlanetas.setAdapter(adaptador);
+
     }
 }
