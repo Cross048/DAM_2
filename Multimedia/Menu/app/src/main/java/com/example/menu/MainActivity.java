87b4df0e2408f5d3d7c4b76bfb728e3e1220924f
Cross048 <cristianbermen@gmail.com>
2024-02-06 11:29:28 +0100

    Menu projects

---
+++ Multimedia/Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvHello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvHello = findViewById(R.id.tvHello);
+        registerForContextMenu(tvHello);
+    }
+
+    // Inflar el menu contextual
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        if (v.getId() == R.id.tvHello) {
+            inflater.inflate(R.menu.menu_contextual, menu); // Asociado a la TextView
+        } else if (v.getId() == R.id.tvHello) {
+
+        }
+    }
+
+    // Listener para le menú contextual
+    @Override
+    public boolean onContextItemSelected(@NonNull Menuitem item) {
+        switch (item.getItemId()) {
+            case R.id.opc_ctx_item1:
+                Toast.makeText(this, "Seleccionado opción 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.opc_ctx_item2:
+                Toast.makeText(this, "Seleccionado opción 2", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+    }
+}
