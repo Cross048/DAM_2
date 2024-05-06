5e32b5e9920b5d5c85d2b39e49b2d4e41daa9294
Cristian Bernal <cristianbermen@gmail.com>
2024-05-06 19:01:46 +0200

    Finished Actividad07

--- Multimedia/RECUPERACION/Actividad07/app/src/main/java/com/pmdm/actividad07/MainActivity.java
+++ Multimedia/RECUPERACION/Actividad07/app/src/main/java/com/pmdm/actividad07/MainActivity.java
@@ -7,9 +7,13 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,6 +34,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setSupportActionBar(findViewById(R.id.toolbar));
+
+        Spinner spinner1 = findViewById(R.id.spinner1);
+        Spinner spinner2 = findViewById(R.id.spinner2);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                // Si se selecciona la opción "6 colores"
+                if (position == 1) {
+                    spinner2.setVisibility(View.VISIBLE); // Hacer spinner2 visible
+                } else {
+                    spinner2.setVisibility(View.GONE); // Hacer spinner2 invisible para otras opciones
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Nada seleccionado
+            }
+        });
+
+        String[] options = {"Sin padding", "Con padding"};
+
+        // Crear un ArrayAdapter utilizando el diseño simple_spinner_item predeterminado de Android
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options);
+
+        // Especificar el diseño a utilizar cuando se despliegan las opciones
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Establecer el adaptador en el Spinner
+        spinner2.setAdapter(adapter);
+
+        Button btnAdelante = findViewById(R.id.btnAdelante);
+        btnAdelante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int opcionSeleccionadaSpinner1 = spinner1.getSelectedItemPosition();
+                int opcionSeleccionadaSpinner2 = spinner2.getSelectedItemPosition();
+
+                Class<?> nextActivity = null;
+
+                // Verificar si la opción seleccionada en spinner2 es la segunda opción y
+                // la opción seleccionada en spinner1 es la segunda opción
+                if (opcionSeleccionadaSpinner2 == 1 && opcionSeleccionadaSpinner1 == 0) {
+                    nextActivity = Activity2.class;
+                }
+                // Verificar si la opción seleccionada en spinner2 es la segunda opción y
+                // la opción seleccionada en spinner1 es la tercera opción
+                else if (opcionSeleccionadaSpinner2 == 1 && opcionSeleccionadaSpinner1 == 1) {
+                    nextActivity = Activity3.class;
+                }
+                // Si spinner2 no está seleccionado,
+                // abrir la actividad basada en la selección de spinner1
+                else {
+                    switch (opcionSeleccionadaSpinner1) {
+                        case 0:
+                            nextActivity = Activity1.class;
+                            break;
+                        case 1:
+                            nextActivity = Activity2.class;
+                            break;
+                        case 2:
+                            nextActivity = Activity3.class;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (nextActivity != null) {
+                    Intent intent = new Intent(MainActivity.this, nextActivity);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
