4be4191239cda0ff93fc24d0753a5a6c463b937f
Cristian Bernal <cristianbermen@gmail.com>
2024-05-15 09:39:37 +0200

    Finished Actividad14

--- Multimedia/RECUPERACION/Actividad14/app/src/main/java/com/pmdm/actividad14/MainActivity.java
+++ Multimedia/RECUPERACION/Actividad14/app/src/main/java/com/pmdm/actividad14/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pmdm.actividad14;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,7 +10,9 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -72,14 +75,77 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // En caso de no seleccionar nada. Método por defecto.
+                // En caso de no seleccionar nada. Método por defecto
             }
         });
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                guardarAlumno();
+                String nombre = txtNombre.getText().toString().trim();
+                if (!nombre.isEmpty()) {
+                    // Método para guardar el Alumno en la Lista
+                    guardarAlumno();
+                } else {
+                    // El usuario no introdujo ningún nombre
+                    Toast.makeText(MainActivity.this,
+                            "Es necesario introducir un nombre", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        listAlumnos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Alumno alumnoSeleccionado = alumnosList.get(position);
+
+                String mensaje = "Nombre: " + alumnoSeleccionado.getNombre() +
+                        "\nCurso: " + alumnoSeleccionado.getCurso();
+                if (alumnoSeleccionado.tieneCiclo()) {
+                    mensaje += "\nCiclo: " + alumnoSeleccionado.getCiclo();
+                }
+
+                // Muestra un Toast con la información del Alumno
+                Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listAlumnos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final Alumno alumnoSeleccionado = alumnosList.get(position);
+
+                // Creamos un AlertDialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(alumnoSeleccionado.getNombre());
+
+                // Definimos las opciones del Dialog
+                String[] opciones = {"Borrar", "Otros"};
+                builder.setItems(opciones, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                // Opción Borrar
+                                alumnosList.remove(alumnoSeleccionado);
+                                alumnosAdapter.notifyDataSetChanged();
+                                Toast.makeText(MainActivity.this,
+                                        "Alumno borrado", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1:
+                                // Opción Otros
+                                Toast.makeText(MainActivity.this,
+                                        "Función no implementada", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                });
+
+                // Crea el AlertDialog y lo muestra al usuario
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
             }
         });
     }
